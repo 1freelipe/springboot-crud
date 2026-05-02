dc3e8e3cd6d4e9a0b15bd053dfe67a427fd06df3
1freelipe <feliperodrigues_spfc@hotmail.com>
2026-05-02 18:22:01 -0300

    último dos métodos do crud adicionado e tratado

--- src/main/java/br/com/fatecararas/dicasdevspring/resources/DicasDevController.java
+++ src/main/java/br/com/fatecararas/dicasdevspring/resources/DicasDevController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -63,6 +64,27 @@ public class DicasDevController {
                 .orElseThrow(() -> new RuntimeException("Dica não encontrada"));
 
         return ResponseEntity.ok().body(entity);
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<DicasDevEntity> edit(@PathVariable Integer id, @RequestBody DicasDevEntity dicasDevEntity) {
+        // Entrando no try e capturando o ID que está vindo nos parâmetro
+        try {
+            DicasDevEntity updateEntity = repository.findById(id)
+                        .orElseThrow(() -> new RuntimeException("ID não encontrado"));
+
+            // Setando os novos atributos na instancia mas capturando os valores que estão vindo no body da requisição
+            updateEntity.setTitulo(dicasDevEntity.getTitulo());
+            updateEntity.setDescricao(dicasDevEntity.getDescricao());
+            repository.save(updateEntity);
+    
+            // Retornando a resposta
+            return ResponseEntity.ok().body(updateEntity);
+        } catch (RuntimeException e) {
+            // Capturando o Runtime se for lançado e logando a mensagem
+            System.err.println(e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/all")
